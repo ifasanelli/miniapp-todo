c2f140b5ccae2980d3adcf051ab772c0e9bd92e6
ifasanelli <fasanelli.italo@gmail.com>
2020-06-19 23:57:07 -0300

    User validation and alert/notice section added to application.html

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: { message: 'Nome não pode ficar em branco' }
 end
